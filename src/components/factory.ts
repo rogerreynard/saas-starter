ea3bdcef5c7eda06a405ad3b677756f6eef0a708
Roger Reynard <Roger.Reynard@hennepin.us>
2026-01-30 14:17:44 -0700

    - Added default elements for Visual Builder

--- src/components/factory.ts
+++ src/components/factory.ts
@@ -1,15 +1,20 @@
 import 'server-only'
-import { getFactory as getBaseFactory, RichTextComponentDictionary } from "@remkoj/optimizely-cms-react/rsc";
+import {
+    getFactory as getBaseFactory,
+    RichTextComponentDictionary,
+    DefaultComponents
+} from "@remkoj/optimizely-cms-react/rsc";
 
-// Import all server components that should be loaded dynamically when the 
+// Import all server components that should be loaded dynamically when the
 // content requires them
 import components from './cms';
 
 // Build the factory statically, so it doesn't need to be rebuild every time
 // it is needed.
 export const factory = getBaseFactory()
-// Register the default components (for rendering Rich Text)
+// Register the default components (Rich Text, Visual Builder elements, etc.)
 factory.registerAll(RichTextComponentDictionary)
+factory.registerAll(DefaultComponents)
 // Register the CMS Components
 factory.registerAll(components)
 
